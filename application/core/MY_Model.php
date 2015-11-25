7bc3003c7e2a9fe3e00f98701aa3951836a48440
daru79 <d.wojcicki@interia.pl>
2015-11-25 21:07:57 +0100

    Podstawowy CRUD

--- application/core/MY_Model.php
+++ application/core/MY_Model.php
@@ -40,4 +40,64 @@ class MY_Model extends CI_Model {
         
     }
     
+    // Pobieranie danych z warunkiem
+    public function get_by($where, $single = FALSE) {
+        $this->db->where($where);
+        
+        return $this->get(NULL, $single);
+        
+    }
+    
+    // Zapisywanie i edycja danych
+    // If you pass an 'id' it will be an update, if not it will be an insert
+    public function save($data, $id = NULL) {
+        // Ustawienie timestamp'a
+        if($this->_timestamps == TRUE) {
+            $now = date('Y-m-d H:i:s'); // Ustawienie bieżacej daty i czasu
+            $id || $data['created'] = $now; // Jeśli '$id' będzie miało wartość 'FALSE' przypisz do zmiennej '$data['created']' zmienną '$now'
+            $data['modified'] = $now;
+            
+        }
+        
+        // Zapisanie nowego rekordu
+        if($id === NULL) {
+            // Dodatkowe sprawdzenie (?) czy jest ustawiona zmienna '$id'
+            !isset($data[$this->_primary_key]) || $data[$this->_primary_key] = NULL; // jeśli jakimś cudem jest ustawiony '$id' jako 'primary key' to przypisz do niego wartość 'NULL'
+            
+            $this->db->set($data); // Ustawiamy wartości
+            $this->db->insert($this->_table_name); // Wprowadzenie danych do danej tabeli
+            $id = $this->db->insert_id(); // Zwraca 'id' wprowadzonego rekordu https://ellislab.com/codeigniter/user-guide/database/helpers.html
+            
+        }
+        
+        // Edycja rekordu
+        else {
+            $filter = $this->_primary_filter; // Ustawiamy filtr
+            $id = $filter($id); // Filtrujemy otrzymane 'id' za pomocą rzutowania na liczbę całkowitą intval()
+            $this->db->set(data); // Ustawiamy wartości
+            $this->db->where($this->_primary_key, $id); // Ustawiamy warunek
+            $this->db->update($this->_table_name); // Modyfikujemy rekord w tabeli na podstawie ww. warunku i dostarczonych danych
+            
+        }
+        
+        return $id;
+        
+    }
+    
+    public function delete($id) {
+        $filter = $this->_primary_filter; // Ustawiamy filtr
+        $id = $filter($id); // Filtrujemy otrzymane 'id' za pomocą rzutowania na liczbę całkowitą intval()
+        
+        // Zabezpieczenie na wypadek jeśli z 'id' byłoby coś nie tak ;)
+        if (!$id) {
+            return FALSE;
+            
+        }
+        
+        $this->db->where($this->_primary_key, $id); // Ustawiamy warunek
+        $this->db->limit(1); // Ustawiamy limit na 1 rekord, aby dodatkowo się upewnić, że zostanie usunięty tylko 1 rekord
+        $this->db->delete($this->_table_name); // Usunięcie rekordu z uwzględnieniem ww. warunków
+        
+    }
+    
 }
